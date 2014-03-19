bc4347180e73ade0343ab9543a4a5aa0fcba43b6
Raphaël Emourgeon <raphael.emourgeon+gitlab@gmail.com>
2014-03-19 18:19:22 +0000

    Add layout

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>TodoMVC</title>
+  {{ HTML::style('//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css') }}
+  {{ HTML::style('/css/main.css') }}
+</head>
+<body>
+  <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">TodoMVC</a>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="/">Home</a></li>
+          <li>{{ link_to_action('TasksController@index', 'Done', array('filter=done')) }}</li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </div>
+  
+	<div class="container">
+			@yield('content')
+	</div>
+</body>
+</html>
